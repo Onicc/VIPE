1bd55aa9b2c591952c9cc93924fae95ac2dca483
Onicc <xinchen.cai@hotmail.com>
2024-10-04 00:12:17 +0800

    Refactor dimensions.py and remove unnecessary code

--- markers_calibration/dimensions.py
+++ markers_calibration/dimensions.py
@@ -1,9 +1,5 @@
 import numpy as np
 
-
-IMU_OFFSET = (0.0, -0.01, 0.004)  # position of IMU relative to the top of the stylus
-STYLUS_LENGTH = 0.1686  # length from the tip to the top of the stylus
-
 def rotateY(angle: float, point: np.ndarray) -> np.ndarray:
     c, s = np.cos(angle), np.sin(angle)
     rotation_matrix = np.array([[c, 0, s], [0, 1, 0], [-s, 0, c]], dtype=np.float32)
@@ -28,7 +24,7 @@ def getCornersPS(
 ) -> np.ndarray:
     cornersWS = getMarkerCorners(markerLength) + origin
     rotated_corners = np.apply_along_axis(lambda x: rotateY(angleY, x), 1, cornersWS)
-    return rotated_corners - IMU_OFFSET
+    return rotated_corners
 
 
 def deg2rad(deg: float) -> float:
